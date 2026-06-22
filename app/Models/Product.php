d69aec6e00f6949894892fa6b971564e5e781cf4
yoooshi4410 <yoshitoakutohayato3@gmail.com>
2026-06-23 07:27:21 +0900

    product_index

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function sales()
+    {
+        return $this->hasMany(Sale::class);
+    }
 }
---
+++ resources/views/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>商品一覧</title>
+</head>
+
+<body>
+    <h1>商品一覧</h1>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>商品番号</th>
+                <th>商品名</th>
+                <th>商品説明</th>
+                <th>画像</th>
+                <th>料金(¥)</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($products as $product)
+            <tr>
+                <td>{{$product->id}}</td>
+                <td>{{$product->product_name}}</td>
+                <td>{{$product->description}}</td>
+                <td>{{$product->img_path}}</td>
+                <td>{{$product->price}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
