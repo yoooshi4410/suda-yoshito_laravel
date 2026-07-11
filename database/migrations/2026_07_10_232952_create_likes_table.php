6c5803f3a2c734e5c9b85dede0afa20d5953b20b
yoooshi4410 <yoshitoakutohayato3@gmail.com>
2026-07-12 07:42:40 +0900

    いいねテーブル追加

---
+++ database/migrations/2026_07_10_232952_create_likes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+
+            //同じユーザーが同じ商品に複数回いいねできないようにユニーク制約
+            $table->unique(['product_id','user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('likes');
+    }
+};
